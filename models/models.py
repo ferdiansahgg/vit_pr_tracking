7b7e4cedaba07c66076796daa1c5b62293e43d14
ferdiansahgg <ferdiayana@gmail.com>
2019-12-04 16:33:11 +0700

    Second Commit

--- models/models.py
+++ models/models.py
@@ -2,14 +2,20 @@
 
 from odoo import models, fields, api
 
-# class vit_pr_tracking(models.Model):
-#     _name = 'vit_pr_tracking.vit_pr_tracking'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class po_tracking(models.Model):
+    _name = "purchase.order.line"
+    _inherit = "purchase.order.line"
+
+    name = fields.Date(string="Tanggal Diterima")
+    proses_pengadaan = fields.Char(string="Proses Pengadaan")
+    reject_pr = fields.Char(string="Reject PR")
+    nomor_po = fields.Integer(string="Nomor PO")
+    tgl_po = fields.Date(string="Tanggal PO")
+    line_po = fields.Char(string="Line PO")
+    nilai_po = fields.Integer(string="Nilai PO")
+    vendor = fields.Char(string="Vendor")
+    bakn = fields.Char(string="BAKN")
+    tgl_share_po = fields.Date(string="Tanggal Share PO")
+    efisiensi_rp = fields.Char(string="Efisiensi RP")
+    efisiensi_percent = fields.Char(string="Efesiensi Percent")
--- models/tracking.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-class tracking(models.Model):
-
-    _name = "purchase.order.line"
-
-    _inherit = "purchase.order.line"
