551168320f3d54dc0755fd9aa00978ed73a2c349
ferdiansahgg <ferdiayana@gmail.com>
2019-12-05 12:54:36 +0700

    Update field

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     "depends": ["base", "vit_product_request"],
     # always loaded
     "data": [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         "views/views.xml",
         "views/templates.xml",
         "views/menu.xml",
--- models/models.py
+++ models/models.py
@@ -7,7 +7,19 @@ class po_tracking(models.Model):
     _name = "purchase.order.line"
     _inherit = "purchase.order.line"
 
-    name = fields.Date(string="Tanggal Diterima")
+    
+    tgl_buat_pr = fields.Data(string="Tanggal Buat PR")
+    nomor_pr  = fields.Integer(string="Nomer PR")
+    deskripsi = fields.Text(string="Deskripsi")
+    wilayah = fields.Char(string="Wilayah")
+    line_pr = fields.Char(string="Line PR")
+    nilai_pr = fields.Integer(string="Nilai PR")
+    diperiksa = fields.Char(string="Diperiksa")
+    konfirmasi_budget = fields.Char(string="Konfirmasi Budget")
+    disetujui = fields.Char(string="Disetujui")
+    
+    
+    tgl_diterima = fields.Date(string="Tanggal Diterima")
     proses_pengadaan = fields.Char(string="Proses Pengadaan")
     reject_pr = fields.Char(string="Reject PR")
     nomor_po = fields.Integer(string="Nomor PO")
@@ -19,3 +31,6 @@ class po_tracking(models.Model):
     tgl_share_po = fields.Date(string="Tanggal Share PO")
     efisiensi_rp = fields.Char(string="Efisiensi RP")
     efisiensi_percent = fields.Char(string="Efesiensi Percent")
+
+    kpi = fields.Char(string="KPI")
+    sla = fields.Char(string="SLA")
